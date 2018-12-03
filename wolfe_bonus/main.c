416acd53f6fedfb6fa345c762dfe49b3d368fc0f
Wolfe <sawppf@delmar.umsl.edu>
2018-12-02 18:45:07 -0600

    square input working

--- wolfe_bonus/main.c
+++ wolfe_bonus/main.c
@@ -1,4 +1,24 @@
+/*
 #Scott Wolfe
 #CS2750 Bonus
 #Date 12/2/2018
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int pid = fork();
+
+	if (pid < 0){
+		perror("Fork has failed!");
+	} else if (pid == 0) {
+		execvp("./square", argv);
+	}
+	wait(NULL);
+	printf("Parent pid: %d ppid: %d date: \n", getpid(), getppid());
+	return 0;
+}
--- wolfe_bonus/square.c
+++ wolfe_bonus/square.c
@@ -1,3 +1,19 @@
+/*
 #Scott Wolfe
 #CS2750 Bonus
 #Date 12/2/2018
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int num;
+	num = atoi(argv[1]);
+
+	printf("%d squared is: %d\n", num, num*num);
+	printf("Child pid: %d ppid: %d date: \n", getpid(), getppid());
+
+	return 0;
+}
